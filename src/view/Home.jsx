bdc28bab9ca4fc647053a45dc27dd00242fd71b5
vorxsaken <jhonyevans6326@gmail.com>
2023-02-02 06:58:52 +0700

    feat: add link for go to post or project view

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -3,35 +3,73 @@ import { motion } from "framer-motion";
 import Card from "../components/Card";
 import Buttons from "../components/Buttons";
 import { useSelector } from "react-redux";
+import { setState } from '../slice/loaderSlice'
+import { useDispatch } from "react-redux"
+import { initEditPost } from "../slice/postSlice"
+import { useNavigate } from "react-router-dom"
+import { initEditProject } from "../slice/projectsSlice";
 
 function Home() {
   const projectsArray = useSelector(state => state.projects.projects);
   const posts = useSelector(state => state.posts.posts);
   const postList = [...posts];
   var sortedPost = postList.sort((a, b) => { return a.created_at - b.created_at });
-  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const goToPost = async (id) => {
+    dispatch(setState(true));
+    fetch(`http://localhost:3010/admin/get-post/${id}`).then(getPost => getPost.json()).then(data => {
+      dispatch(initEditPost({ _id: data._id, title: data.title, subtitle: data.subtitle, text: data.text, tags: data.tags, imageHeader: data.imageHeader, created_at: data.created_at }))
+      setTimeout(() => {
+        navigate("/post-view");
+        dispatch(setState(false));
+      }, 400)
+    })
+  }
+
+  const goToProject = async (id) => {
+    dispatch(setState(true));
+    fetch(`http://localhost:3010/admin/get-project/${id}`).then(getProject => getProject.json()).then(data => {
+      dispatch(initEditProject({
+        _id: data[0]._id,
+        title: data[0].title,
+        github: data[0].github,
+        techStack: data[0].techStack,
+        deskripsi: data[0].deskripsi,
+        imageHeader: data[0].imageHeader,
+      }))
+
+      setTimeout(() => {
+        navigate("/project-view");
+        dispatch(setState(false));
+      }, 400)
+    })
+  }
   // posts cards
   const cards = sortedPost.slice(0, 4).map((post) => (
-      <Card
-        key={post._id}
-        img={post.imageHeader}
-        title={post.title}
-        des={post.subtitle}
-        tags={post.tags}
-        row={true}
-        date={post.created_at}
-      />
+    <Card
+      onClick={() => goToPost(post._id)}
+      key={post._id}
+      img={post.imageHeader}
+      title={post.title}
+      des={post.subtitle}
+      tags={post.tags}
+      row={true}
+      date={post.created_at}
+    />
   ));
-  
+
   // project cards
   const projects = projectsArray.slice(0, 4).map((project) => (
-      <Card
-        key={project._id}
-        img={project.imageHeader.pic[0].url}
-        row={false}
-        title={project.title}
-        des={project.deskripsi}
-      />
+    <Card
+      onClick={() => goToProject(project._id)}
+      key={project._id}
+      img={project.imageHeader.pic[0].url}
+      row={false}
+      title={project.title}
+      des={project.deskripsi}
+    />
   ));
 
   useEffect(() => {
@@ -83,11 +121,11 @@ function Home() {
       {/* content posts list summary */}
       <div className="w-full h-auto flex flex-col gap-2 bg-white mt-4">
         <motion.p
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        viewport={{ once: true }}
-        transition={{ delay: 0.3, duration: 1.2, type: "spring" }}
-        className="text-center font-bold text-2xl text-slate-600 mt-10 relative">
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ delay: 0.3, duration: 1.2, type: "spring" }}
+          className="text-center font-bold text-2xl text-slate-600 mt-10 relative">
           <span
             className="before:w-40 before:h-[3px] before:-bottom-1 before:absolute 
           before:rounded-xl before:bg-gradient-to-r before:from-red-400"
@@ -104,7 +142,7 @@ function Home() {
           {cards}
         </motion.div>
         <div className="py-4 flex flex-row items-center justify-center">
-          <span>
+          <span onClick={() => { navigate("/posts") }}>
             <Buttons text="More .." />
           </span>
         </div>
@@ -112,7 +150,7 @@ function Home() {
 
       {/* constent project list summary */}
       <div className="w-full h-auto flex flex-col gap-2 bg-white">
-        <motion.p 
+        <motion.p
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
@@ -134,9 +172,9 @@ function Home() {
           {projects}
         </motion.div>
         <div className="py-4 flex flex-row items-center justify-center">
-        <div>
+          <span onClick={() => { navigate("/projects")}}>
             <Buttons text="More .." />
-          </div>
+          </span>
         </div>
       </div>
     </motion.div>
